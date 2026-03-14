617175c016b5854d2ce46ee72ef30a9960bdeddf
Amit Dutta <amitdutta4255@gmail.com>
2026-03-14 22:23:18 +0530

    Initialize Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // This tells Tailwind to scan your index.html!
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
